4556061986e2958ac4e7fd6656f8ca8aaf013e86
zhanghaojie <1455123087@qq.com>
2025-06-11 15:46:54 +0800

    Export extend and select functions in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ export * from "./createDefaultStore";
 export * from "./createEntityStateStore";
 export * from "./createStoreContext";
 export * from "./exports";
+export * from "./extend";
+export * from "./select";
 export * from "./typings";
