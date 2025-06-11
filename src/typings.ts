5a03ee26e362cab62ff5a6a36eab56ba58c80eac
zhanghaojie <1455123087@qq.com>
2025-06-11 15:46:10 +0800

    Add InferStoreState type to improve type inference for StoreApi

--- src/typings.ts
+++ src/typings.ts
@@ -5,3 +5,5 @@ export type AnyObject = Record<string, any>;
 export type WithOptionalStore<Props, State> = Props & {
   store?: StoreApi<State>;
 };
+
+export type InferStoreState<T> = T extends StoreApi<infer S> ? S : never;
